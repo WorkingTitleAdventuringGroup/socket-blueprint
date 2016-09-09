b56d343acd2399a21c0035ccebc87975ae461154
Jacob Gardner <jacob.v.gardner@gmail.com>
2016-09-08 22:31:53 -0500

    More project setup

---
+++ Gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const rollup = require('rollup-stream');
+const babel = require('rollup-plugin-babel');
+const notify = require('gulp-notify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const sourcemaps = require('gulp-sourcemaps');
+
+const SOURCE_FILES = ['./lib/**/*.js'];
+
+gulp.task('scripts', () => {
+    rollup({
+        entry: './lib/main.js',
+        sourceMap: true,
+        format: 'cjs',
+        plugins: [
+            babel({
+                exclude: 'node_modules/**',
+            }),
+        ],
+    })
+    .on('error', function (err) {
+        notify.onError({
+            title: 'Source Build Error',
+            message: '<%= error.message %>',
+            sound: 'Bottle',
+        })(err);
+
+        console.error(err.stack);
+        this.emit('end');
+    })
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(notify({
+        onLast: true,
+        message: 'Successfully Built',
+    }));
+});
+
